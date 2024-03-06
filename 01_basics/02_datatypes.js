ddb9352f27224b096e381dc0bfa0f306b6551288
seeratfatema <115491132+seeratfatema@users.noreply.github.com>
2024-03-07 00:18:46 +0530

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+console.log(typeof undefined);           //   treat all JS code as  newer versionconsole.log(typeof undefined)
+ 
+alert(3+3)                              //not in JS
+ 
+
+console.log(
+    3+
+    3)                                   //code readability should be high
+
+
+let name="Seerat"            //string datatype
+let age=18                   //number datatype
+let isLoggedIn=False         //boolean datatype
+
+/* -----------PRIMITIVE DATATYPE---------*/
+//number=>range 2 to the power 53
+// bigint => use for large numbers like in trading /stock market
+//string=""
+// boolean=> true/false
+//null=> standalonevalue
+//undefined => when value is not assigned
+// symbol=> unique
+
+
+/* How to find type of*/
+console.log(typeof "Seerat");       //string
+console.log(typeof age );           //number
+console.log(typeof null);           //object
+console.log(typeof undefined);      //undefined
+     
